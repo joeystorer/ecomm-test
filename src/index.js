0cdb23ed0a59aa953ad5e8fae3a99fdc4d950bc9
Joey Storer <joeystorer@gmail.com>
2022-01-24 12:12:50 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-let element = < div > Hello World < /div>;
+let element = <div> Hello World </div>;
 ReactDOM.render(element, document.getElementById("root"));
